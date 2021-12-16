f6be4fd621f01b5a5f15f35e4ccd0c48099b3da8
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-16 09:21:20 +0100

    updating index.server.js

--- index.server.js
+++ index.server.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = 9000;
 
-app.get("/song", (req, res) => {
-    res.send("Hello from server");
+const __AUDIO_TYPE__ = {
+    ROCK: "rock",
+    JAZZ: "jazz",
+    CINEMATIC: "cinematic",
+    AFROBEAT: "afrobeat",
+    JUJU: "juju",
+    ARTISTE: "artiste"
+}
+
+function getDirectories(type){
+    return fs.readdirSync(`./music/${type}`);
+}
+
+
+
+// const __ARTISTE_TYPE__ = {
+//     ARTISTE: "artiste"
+// }
+
+// app.get(`/list`, (req, res) => {
+
+// })
+
+app.get(`/song`, (req, res) => {
+    for(let type in __AUDIO_TYPE__) {
+        console.log(getDirectories(__AUDIO_TYPE__[type]));
+    }
+    res.send("hello from server")
 });
 
 app.listen(PORT, () => {

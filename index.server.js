c55664726e7ca29d651e4787810cdbaa643229fb
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-16 09:36:37 +0100

    updates on index.sever.js

--- index.server.js
+++ index.server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
+const { appData } = require("./backend-music/mock");
 const app = express();
 const PORT = 9000;
 
@@ -16,23 +18,45 @@ function getDirectories(type){
     return fs.readdirSync(`./music/${type}`);
 }
 
+function getSongDetails(type, dir){
+    try{
+        const path = `music/${type}/${dir}/media`;
+        const audioData = fs.readdirSync(path);
+        const audioInfo = fs.readFileSync(`./music/${type}/${dir}/info.json`);
+        const parsedData = JSON.parse(audioInfo);
+        return {
+            audioFile: `${path}/${audioData[0]}`,
+            avatar: `${path}/${audioData[1]}`,
+            ...parsedData
+        }
+    }catch(error){
+        return false;
+    }
+}
 
-
-// const __ARTISTE_TYPE__ = {
-//     ARTISTE: "artiste"
-// }
-
-// app.get(`/list`, (req, res) => {
-
-// })
+app.use(cors());
+app.use('/music', express.static('music'));
 
 app.get(`/song`, (req, res) => {
-    for(let type in __AUDIO_TYPE__) {
-        console.log(getDirectories(__AUDIO_TYPE__[type]));
+    const songData = {};
+    for(let type in __AUDIO_TYPE__){
+        const directoryItems = getDirectories(__AUDIO_TYPE__[type]);
+        directoryItems.forEach((item) => {
+            const audioData = getSongDetails(__AUDIO_TYPE__[type], item);
+            if(audioData){
+                if(__AUDIO_TYPE__[type] in songData){
+                    songData[__AUDIO_TYPE__[type]].push(audioData);
+                }else{
+                    songData[__AUDIO_TYPE__[type]] = [audioData];
+                }
+            }            
+        })
     }
-    res.send("hello from server")
+    appData['freelicense'] = songData
+
+    res.status(200).json({ appData });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening to port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 })
